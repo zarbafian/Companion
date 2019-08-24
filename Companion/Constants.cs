306e6a42d69a411367e87c10f115ca7da67a24bc
pouriya-zarbafian <pouriya.zarbafian@gmail.com>
2019-08-24 04:00:39 +0200

    server URL

--- Companion/Constants.cs
+++ Companion/Constants.cs
@@ -3,7 +3,8 @@ namespace Companion
 {
     public static class Constants
     {
-        public static string BackboneEndpoint = "http://localhost:8080";
+        //public static string BackboneEndpoint = "http://localhost:8080";
+        public static string BackboneEndpoint = "http://167.71.61.110:8080";
         public static string ApiToken = "lk1waRs4lHQbQyM3jPBfSaVEsJ9nsx+EtjRNnXQG+Rw=";
     }
 }
